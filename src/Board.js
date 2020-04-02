20c4d22844aae294157b1cb3af60389fa84d5a9b
aloysiuslai <aloysiuslai13@gmail.com>
2020-04-01 20:15:57 -0700

    Implemented Board.js

--- src/Board.js
+++ src/Board.js
@@ -79,12 +79,50 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      // Check if row array have multiple 1s
+      /**
+       * IOCE
+       * input: number; the index of the row;
+       * output: boolean; if more than two 1's returns true else return false
+       *
+       * this.attribute = { 0 : arr1,
+       *                    1 : arr2,
+       *                    2 : arr3
+       *                  }
+       *
+       */
+
+       var count = 0;
+       for ( var key in this.attributes) {
+        if ( key === rowIndex) {
+          var row = this.attributes[key];
+          for ( var num of row) {
+            if ( num === 1) {
+              count++;
+            }
+          }
+        }
+      }
+      return (count > 1)
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      /* -----------------------------------------------------
+      IOCE
+      I: N/A
+      O: boolean depending if all row return true or false
+      find out how many keys there are in this.attributes;
+      call this.hasRowConflictAt(different keys here)
+
+      E: if key = 'n'
+      */
+
+      for ( var arr in this.attributes) {
+        if ( arr === "n") continue;
+        if (this.hasRowConflictAt(arr)) return true;
+      }
+      return false;
     },
 
 
@@ -94,12 +132,42 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
+      /* -----------------------------------------------------------
+      IOCE
+      I: number, to indicate what column of the matrix we are iterating through
+      O: boolean; if more than two 1's in a column return true;
+      C: N/A
+      E: TBD;
+
       return false; // fixme
+
+      create a count var;
+      loop though this.attributes;
+      we in each attribute; count++ when key ==1
+      if count is greater than 1, then there's conflict
+      */
+     var count = 0;
+
+      for ( var key in this.attributes) {
+        // do an if, so this.attributes[key][colIndex]] === 1, count ++
+        if (this.attributes[key][colIndex] === 1) count++;
+      }
+      return count > 1;
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-      return false; // fixme
+      // return false; // fixme
+      /**
+       * input: N/A;
+       * output: boolean; if there are any col conflicts in the martix;
+       * edge case: when this.attributes[key] === 'n', then continue;
+       */
+      for ( var arr in this.attributes) {
+        if ( arr === "n") continue;
+        if (this.hasColConflictAt(arr)) return true;
+      }
+      return false;
     },
 
 
@@ -108,13 +176,59 @@
     // --------------------------------------------------------------
     //
     // test if a specific major diagonal on this board contains a conflict
-    hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+    hasMajorDiagonalConflictAt: function(startingI,startingJ, n) {
+      var count = 0;// 1
+      var i = startingI;// i = 3, n = 4
+      var j = startingJ;// j = 0
+
+      while ( true ) {
+        if (i === n || j === n) {// 3 !== 4
+          break;
+        }
+        if ( this.attributes[i][j] === 1) {
+          count++;
+        }
+        i++;// i = 4
+        j++;// j = 1
+      }
+      return count > 1
+      // return false; // fixme
+      /* IOCE
+        I: i and j for coordinate location
+        O: boolean to determine conflicts
+
+        i would the key of this.attributes, j would be index of this.attributes[key]
+        Compare the element(0 or 1) at this.attributes[i][j]
+        if element is 1, then add to count
+        move to next element i++ and j++ -> this.attributes[i++][j++], if i or j = n - 1, return count > 1;
+       */
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      // return false; // fixme
+      /**
+       * step 1; to start from the bottom left; i : n - 1 and j : 0; then call the function
+       * step 2 : i-- loop till i = 0 then move right (j++)
+       * step 3 : return count > 1;
+       */
+      var n = this.attributes[0].length;
+      var i = n - 1;
+      var j = 0;
+
+      while (true) {
+        if ( i !== 0) {
+          if (this.hasMajorDiagonalConflictAt(i, j , n)) return true;
+          i--;
+        } else if ( j !== n - 1) {
+          if (this.hasMajorDiagonalConflictAt(i, j , n)) return true;
+          j++;
+        } else {
+          if (this.hasMajorDiagonalConflictAt(i, j , n)) return true;
+          break;
+        }
+      }
+      return false;
     },
 
 
@@ -123,13 +237,43 @@
     // --------------------------------------------------------------
     //
     // test if a specific minor diagonal on this board contains a conflict
-    hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+    hasMinorDiagonalConflictAt: function(startingI, startingJ, n) {
+      var count = 0;// 1
+      var i = startingI;// i = 3, n = 4
+      var j = startingJ;// j = 0
+
+      while ( true ) {
+        if (i === n || j === n) {// 3 !== 4
+          break;
+        }
+        if ( this.attributes[i][j] === 1) {
+          count++;
+        }
+        i++;//
+        j--;// j = 1
+      }
+      return count > 1
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      var n = this.attributes[0].length;
+      var i = n - 1;
+      var j = n - 1;
+
+      while (true) {
+        if ( i !== 0) {
+          if (this.hasMinorDiagonalConflictAt(i, j , n)) return true;
+          i--;
+        } else if ( j !== 0) {
+          if (this.hasMinorDiagonalConflictAt(i, j , n)) return true;
+          j--;
+        } else {
+          if (this.hasMinorDiagonalConflictAt(i, j , n)) return true;
+          break;
+        }
+      }
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
@@ -146,3 +290,4 @@
   };
 
 }());
+
